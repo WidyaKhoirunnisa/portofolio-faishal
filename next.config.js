dee94b21c7eef35a11ecc91ffe87fe6e2fc8d7ad
macbook <macbook@macbooks-MacBook-Pro-4.local>
2025-09-26 13:38:11 +0700

    next.config.js package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // Mode static export
+  output: 'export',
+
+  // Opsional jika mau:
+  // trailingSlash: true,              // URL diakhiri slash
+  // skipTrailingSlashRedirect: true,  // Tidak redirect otomatis
+  // distDir: 'dist',                  // Ganti nama folder output
 };
 
 module.exports = nextConfig;
