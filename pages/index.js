6b8edbe3231263c8b00371ec2f1805ce7a4e6e09
macbook <macbook@macbooks-MacBook-Pro-4.local>
2025-09-26 14:13:11 +0700

    remove index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Halo, ini halaman Next.js pertama!
-      </h1>
-    </main>
-  );
-}
